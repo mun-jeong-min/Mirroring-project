61142e61b3c38560ec6a1487f8a34603d12857d8
문정민 <moondeve41@gmail.com>
2023-05-11 14:04:06 +0900

    ADD :: cors, properties config

---
+++ src/main/java/com/example/companyproject/global/config/PropertiesConfig.java
@@ -0,0 +1,9 @@
+package com.example.companyproject.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationPropertiesScan("com.example.companyproject")
+@Configuration
+public class PropertiesConfig {
+}
