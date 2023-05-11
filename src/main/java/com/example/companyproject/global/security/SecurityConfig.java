d8980fd4656f59eef81fb36665e39248d7ecdd93
문정민 <moondeve41@gmail.com>
2023-05-11 14:04:41 +0900

    ADD :: SecurityConfig

---
+++ src/main/java/com/example/companyproject/global/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.companyproject.global.security;
+
+import com.example.companyproject.global.security.jwt.JwtTokenProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final ObjectMapper objectMapper;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .authorizeRequests()
+
+                .antMatchers(HttpMethod.GET, "/user").permitAll()
+
+                .anyRequest().authenticated()
+                .and()
+                .apply(new FilterConfig(objectMapper, jwtTokenProvider));
+
+        return http.build();
+    }
+}
